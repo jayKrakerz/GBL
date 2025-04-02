6e9d8b638a4d8b67454528570c8e3a3ab8323c3e
jayKrakerz <jimkeyz2015@gmail.com>
2025-04-02 16:49:08 +0000

    pulled from dev

--- assets/product-model.js
+++ assets/product-model.js
@@ -1,14 +1,12 @@
 if (!customElements.get('product-model')) {
-  customElements.define(
-    'product-model',
-    class ProductModel extends DeferredMedia {
+    customElements.define('product-model', class ProductModel extends DeferredMedia {
       constructor() {
         super();
       }
-
+  
       loadContent() {
         super.loadContent();
-
+  
         Shopify.loadFeatures([
           {
             name: 'model-viewer-ui',
@@ -17,43 +15,45 @@ if (!customElements.get('product-model')) {
           },
         ]);
       }
-
+  
       setupModelViewerUI(errors) {
         if (errors) return;
-
+  
         this.modelViewerUI = new Shopify.ModelViewerUI(this.querySelector('model-viewer'));
       }
-    }
-  );
-}
-
-window.ProductModel = {
-  loadShopifyXR() {
-    Shopify.loadFeatures([
-      {
-        name: 'shopify-xr',
-        version: '1.0',
-        onLoad: this.setupShopifyXR.bind(this),
-      },
-    ]);
-  },
-
-  setupShopifyXR(errors) {
-    if (errors) return;
-
-    if (!window.ShopifyXR) {
-      document.addEventListener('shopify_xr_initialized', () => this.setupShopifyXR());
-      return;
-    }
-
-    document.querySelectorAll('[id^="ProductJSON-"]').forEach((modelJSON) => {
-      window.ShopifyXR.addModels(JSON.parse(modelJSON.textContent));
-      modelJSON.remove();
     });
-    window.ShopifyXR.setupXRElements();
-  },
-};
-
-window.addEventListener('DOMContentLoaded', () => {
-  if (window.ProductModel) window.ProductModel.loadShopifyXR();
-});
+  }
+  
+  window.ProductModel = {
+    loadShopifyXR() {
+      Shopify.loadFeatures([
+        {
+          name: 'shopify-xr',
+          version: '1.0',
+          onLoad: this.setupShopifyXR.bind(this),
+        },
+      ]);
+    },
+  
+    setupShopifyXR(errors) {
+      if (errors) return;
+  
+      if (!window.ShopifyXR) {
+        document.addEventListener('shopify_xr_initialized', () =>
+          this.setupShopifyXR()
+        );
+        return;
+      }
+  
+      document.querySelectorAll('[id^="ProductJSON-"]').forEach((modelJSON) => {
+        window.ShopifyXR.addModels(JSON.parse(modelJSON.textContent));
+        modelJSON.remove();
+      });
+      window.ShopifyXR.setupXRElements();
+    },
+  };
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    if (window.ProductModel) window.ProductModel.loadShopifyXR();
+  });
+  
